526175aeae29fe08a628fedb3e1a6bfa70a88b6b
Brian-Machiestay <brianmachiestay@gmail.com>
2023-02-05 12:11:10 +0000

    check if all boxes can be opened

---
+++ 0x00-pascal_triangle/0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""given a number of boxes, write a program to check if all boxes can be opened
+"""
+
+def canUnlockAll(boxes):
+    """check if all boxes can be opened"""
+    boxState = []
+    for i in range(len(boxes)):
+        for box in boxes:
+            for key in box:
+                if key == i:
+                    boxState.append(True)
+                    break
+            if len(boxState) == i + 1:
+                break
+        if len(boxState) == i + 1:
+            continue
+        boxState.append(False)
+
+    boxNum = 0
+    boxState[boxNum] = True
+    for box in boxes:
+        if boxState[boxNum] == True:
+            for key in box:
+                if key < len(boxes):
+                    boxState[key] = True
+        boxNum += 1
+    for box in boxState:
+        if box == [False]:
+            return False
+    return True
