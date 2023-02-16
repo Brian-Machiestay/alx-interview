5df92a01c0afc74303ad8f70e345f51099d07706
Brian-Machiestay <brianmachiestay@gmail.com>
2023-02-16 22:24:48 +0000

    correct pycodestyle errors

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -5,34 +5,41 @@
 import re
 import sys
 
+
 def validate_input(line):
     """validate the line"""
-    regx_addr = '^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+'
-    regx_date = '\[[0-9]{4}-[0-9]{2}-[0-9]{2}\s[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]+\]'
-    regx_get = '\"GET\s\/projects\/260\sHTTP\/1.1\"'
-    regx_status = '\s[0-9]{3}\s'
-    regx_size = '[0-9]+$'
-    x = regx_addr + '\s-\s' + regx_date + '\s' + regx_get + regx_status + regx_size
+    regx_addr = r'^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+'
+    regx_date = r'\[[0-9]{4}-[0-9]{2}-[0-9]{2}' +\
+        r'\s[0-9]{2}:[0-9]{2}:[0-9]{2}\.[0-9]+\]'
+    regx_get = r'\"GET\s\/projects\/260\sHTTP\/1.1\"'
+    regx_status = r'\s[0-9]{3}\s'
+    regx_size = r'[0-9]+$'
+    x = regx_addr + r'\s-\s' + regx_date + r'\s' +\
+        regx_get + regx_status + regx_size
     x = re.compile(x)
     x = x.match(line)
     if x is None:
         return False
     return True
 
+
 def get_file_size(line):
-    regx_size = '[0-9]+$'
+    """get filesize of this input"""
+    regx_size = r'[0-9]+$'
     x = re.search(regx_size, line)
     return(int(x.group()))
 
 
-
 def get_status(line):
-    regx_status = '\s[0-9]{3}\s'
+    """get the status of this line"""
+    regx_status = r'\s[0-9]{3}\s'
     x = re.search(regx_status, line)
-    x = re.search('[0-9]{3}', x.group())
+    x = re.search(r'[0-9]{3}', x.group())
     return(x.group())
 
+
 def print_info(size, stat):
+    """print the stored infomation so far"""
     print('File size: {}'.format(size))
     status = [200, 301, 400, 401, 403, 404, 405, 500]
     for i in status:
@@ -42,7 +49,8 @@ def print_info(size, stat):
 
 if __name__ == "__main__":
     line_number = 0
-    stat = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
+    stat = {"200": 0, "301": 0, "400": 0,
+            "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
     totalsize = 0
     try:
         for line in sys.stdin:
