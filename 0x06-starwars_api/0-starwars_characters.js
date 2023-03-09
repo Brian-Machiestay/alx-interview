4047ea082c47af906472c0ce380f5c3de86ae8d4
Brian-Machiestay <brianmachiestay@gmail.com>
2023-03-09 20:18:22 +0000

    star was characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -2,29 +2,31 @@
 // starwars api
 
 const request = require('request');
-const arg = process.argv[2]
+const arg = process.argv[2];
 
-async function ret_pro(url) {
-  return new Promise(function(resolve, reject) {
-    request(url, function(err, res, body) {
+async function retPro (url) {
+  return new Promise(function (resolve, reject) {
+    request(url, function (err, res, body) {
       resolve(JSON.parse(body).name);
-    })
-  })
+      if (err) throw err;
+    });
+  });
 }
 
-async function chars() {
-  return new Promise(function(resolve, reject) {
-    request(`https://swapi-api.alx-tools.com/api/films/${arg}`,function(err, res, bod) {
+async function chars () {
+  return new Promise(function (resolve, reject) {
+    request(`https://swapi-api.alx-tools.com/api/films/${arg}`, function (err, res, bod) {
       resolve(JSON.parse(bod).characters);
-    })
-  })
+      if (err) throw err;
+    });
+  });
 }
 
-async function names() {
-  thischars = await chars()
-  for(let i = 0; i < thischars.length; i++) {
-    console.log(await ret_pro(thischars[i]))
+async function names () {
+  const thischars = await chars();
+  for (let i = 0; i < thischars.length; i++) {
+    console.log(await retPro(thischars[i]));
   }
 }
 
-names()
+names();
