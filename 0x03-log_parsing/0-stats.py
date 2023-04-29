548742c27aed9c6584c3f41a4e32c67ac9114604
Brian-Machiestay <brianmachiestay@gmail.com>
2023-04-29 07:59:28 +0000

    check full log

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -64,6 +64,7 @@ if __name__ == "__main__":
             line_number += 1
             if line_number == 11:
                 print_info(totalsize, stat)
+                line_number = 1
         if not sys.stdin.isatty():
             print_info(totalsize, stat)
     except KeyboardInterrupt:
